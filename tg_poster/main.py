28f570f9d22d78b4e05384b637b0f2e0cf2f58cf
tony <piton20091986@gmail.com>
2021-06-20 08:25:59 +0400

    mod_publisher

--- tg_poster/main.py
+++ tg_poster/main.py
@@ -2,7 +2,7 @@ import config as conf
 import requests
 import time
 
-def get_new_posts(post_api_url, over_time, tg_url, chanel_id):
+def get_new_posts(post_api_url, over_time, tg_url, chanel_id, base_dir):
 
     response = requests.get(post_api_url)
     res = response.json()
@@ -16,18 +16,15 @@ def get_new_posts(post_api_url, over_time, tg_url, chanel_id):
 
         conf.POST_ID = res_id
 
-        title = res[0]['post_title']
-        body = res[0]['post_body']
-        img_path = res[0]['post_img']
-        slug = res[0]['post_slug']
+        slug = base_dir + "/" + res[0]['post_slug']
 
-        message = "НАЗВАНИЕ: {} \nСОДЕРЖИМОЕ:\n{}".format(title, body)
+        message = "Ссылка на ресурс: {}".format(slug)
 
         publish_from_tg_chanel(chanel_id, tg_url, message)
                 
 
 def publish_from_tg_chanel(chanel_id, tg_url, message):
-
+    
     params = {'chat_id': chanel_id, 'text': message}
     response = requests.get(tg_url + 'sendMessage', data=params)
 
@@ -37,11 +34,12 @@ if __name__ == "__main__":
 
     while True:
 
-        time.sleep(15)
+        time.sleep(5)
 
         get_new_posts(
             conf.URL_POST_API,
             conf.OVER_TIME,
             conf.URL_TG_API,
             conf.CHANEL_ID,
+            conf.BASE_DIR
         )
